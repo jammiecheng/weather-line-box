dfe79a2bb7d558af7c6607c3ddd5472106d29743
jjw118 <jun910811@gmail.com>
2022-11-13 23:20:15 +0800

    init

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def get_data(city):
     res = [[] , [] , []]
     for j in range(3):
         for i in Data:
-            res[j].append(j['time'][i])
+            res[j].append(i['time'][j])
     return res
 
 if __name__ == "__main__":
